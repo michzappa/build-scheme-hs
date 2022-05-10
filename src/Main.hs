c48ab64468867b9e7290cd6d30918df63661ad79
Michael Zappa <me@michzappa.com>
2022-05-10 10:34:06 -0400

    REPL

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import           Control.Monad
 import           Control.Monad.Except
 import           System.Environment
+import           System.IO
 import           Text.ParserCombinators.Parsec hiding (spaces)
 
 -- DATA STRUCTURES/TYPES
@@ -284,9 +285,34 @@ eval (List [Atom "if", pred, conseq, alt]) = do
 eval (List (Atom func:args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
--- MAIN
+-- IO
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $
+  extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+   result <- prompt
+   if pred result
+      then return ()
+      else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
 main :: IO ()
-main = do
-  args <- getArgs
-  evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+main = do args <- getArgs
+          case length args of
+               0         -> runRepl
+               1         -> evalAndPrint $ args !! 0
+               otherwise -> putStrLn "Program takes only 0 or 1 argument"
